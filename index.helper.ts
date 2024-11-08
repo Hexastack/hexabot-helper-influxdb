20e0336ddd9660841cdce5ec5772dfe911cf0044
yassinedorbozgithub <yassine.dorboz.2022@gmail.com>
2024-11-08 15:42:25 +0100

    fix: replace Block type by BlockFull type

--- index.helper.ts
+++ index.helper.ts
@@ -13,7 +13,7 @@ import slug from 'slug';
 
 import { BotStatsType } from '@/analytics/schemas/bot-stats.schema';
 import EventWrapper from '@/channel/lib/EventWrapper';
-import { Block, BlockFull } from '@/chat/schemas/block.schema';
+import { BlockFull } from '@/chat/schemas/block.schema';
 import { Subscriber } from '@/chat/schemas/subscriber.schema';
 import { Context } from '@/chat/schemas/types/context';
 import { OutgoingMessage } from '@/chat/schemas/types/message';
@@ -537,7 +537,7 @@ export default abstract class InfluxdbHelper
    */
   public logPluginEvent(
     pluginTitle: string,
-    block: Block,
+    block: BlockFull,
     context: Context,
     extraFields: { [key: string]: any },
   ) {
