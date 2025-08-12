5a0c231c4b3bfcace8325ee11b46188859fcffc8
Mohamed Marrouchi <marrouchi.mohamed@gmail.com>
2025-08-12 10:10:35 +0100

    feat: custom subject tagname

--- index.helper.ts
+++ index.helper.ts
@@ -1,9 +1,12 @@
 /*
- * Copyright © 2024 Hexastack. All rights reserved.
+ * Copyright © 2025 Hexastack. All rights reserved.
  *
- * Licensed under the GNU Affero General Public License v3.0 (AGPLv3) with the following additional terms:
+ * This software is licensed under a proprietary license.
+ * Unauthorized copying, distribution, modification, or use of this software, in whole or in part, is strictly prohibited without prior written permission from Hexastack.
+ *
  * 1. The name "Hexabot" is a trademark of Hexastack. You may not use this name in derivative works without express written permission.
  * 2. All derivative works must include clear attribution to the original creator and software, Hexastack and Hexabot, in a prominent location (e.g., in the software's "About" section, documentation, and README file).
+ * 3. Use of this software is restricted to authorized licensees only and subject to the terms of the licensing agreement provided by Hexastack.
  */
 
 import { InfluxDB, Point } from '@influxdata/influxdb-client';
@@ -383,13 +386,17 @@ export default class InfluxdbHelper
     block: BlockFull,
     context: Context,
   ) {
+    const settings = await this.getSettings();
     const subscriber = event.getSender();
+    const subjectKey = settings.subject_tagname || 'subject';
     const tags = {
       ...this.getMessageTags(event),
       channel: event._handler.getName() || 'unknown',
       type: 'block',
-      subject: await this.getBlockSubject(block.name),
+      [subjectKey]: await this.getBlockSubject(block.name),
+      category: block.category?.label || 'unknown',
     };
+
     const fields: InfluxFields = {
       ...this.getSubscriberFields(subscriber),
       ...this.getBlockFields(event, block, context),
--- settings.ts
+++ settings.ts
@@ -1,9 +1,12 @@
 /*
- * Copyright © 2024 Hexastack. All rights reserved.
+ * Copyright © 2025 Hexastack. All rights reserved.
  *
- * Licensed under the GNU Affero General Public License v3.0 (AGPLv3) with the following additional terms:
+ * This software is licensed under a proprietary license.
+ * Unauthorized copying, distribution, modification, or use of this software, in whole or in part, is strictly prohibited without prior written permission from Hexastack.
+ *
  * 1. The name "Hexabot" is a trademark of Hexastack. You may not use this name in derivative works without express written permission.
  * 2. All derivative works must include clear attribution to the original creator and software, Hexastack and Hexabot, in a prominent location (e.g., in the software's "About" section, documentation, and README file).
+ * 3. Use of this software is restricted to authorized licensees only and subject to the terms of the licensing agreement provided by Hexastack.
  */
 
 import { HelperSetting } from '@/helper/types';
@@ -24,30 +27,42 @@ export default [
     group: INFLUXDB_HELPER_NAMESPACE,
     label: 'token',
     value: 'mytoken',
-    type: SettingType.text,
+    type: SettingType.secret,
+    translatable: false,
   },
   {
     group: INFLUXDB_HELPER_NAMESPACE,
     label: 'organization',
     value: 'Hexastack',
     type: SettingType.text,
+    translatable: false,
   },
   {
     group: INFLUXDB_HELPER_NAMESPACE,
     label: 'bucket',
     value: 'Hexabot',
     type: SettingType.text,
+    translatable: false,
   },
   {
     group: INFLUXDB_HELPER_NAMESPACE,
     label: 'subjects',
     value: ['Greeting', 'Question', 'Handover'],
     type: SettingType.multiple_text,
+    translatable: false,
   },
   {
     group: INFLUXDB_HELPER_NAMESPACE,
     label: 'default_subject',
     value: 'Other',
     type: SettingType.text,
+    translatable: false,
+  },
+  {
+    group: INFLUXDB_HELPER_NAMESPACE,
+    label: 'subject_tagname',
+    value: 'subject',
+    type: SettingType.text,
+    translatable: false,
   },
 ] as const satisfies HelperSetting<typeof INFLUXDB_HELPER_NAME>[];
